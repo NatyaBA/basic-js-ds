694b97b6f8625850d266c02c3b5f98cf96157f72
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2022-10-17 21:04:06 +0300

    Add files via upload

---
+++ basic-js-ds/src/queue.js
@@ -0,0 +1,45 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+// const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+class Queue {
+start =null;
+  getUnderlyingList() {
+    return this.start;
+  }
+
+  enqueue(value) {
+    if (!this.start) {
+      this.start = new ListNode(value);
+    } else {
+      let list = this.start;
+      while (list.next) {
+        list = list.next;
+      }
+      list.next = new ListNode(value);
+    }
+  }
+
+  dequeue() {
+  	if (this.start) {
+      const value = this.start.value;
+      this.start = this.start.next;
+      return value;
+    }
+  }
+}
+
+module.exports = {
+  Queue
+};
